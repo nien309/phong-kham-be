02151ab786e3b9821973348130e15f693051efff
yentn-work <yentn.work@gmail.com>
2025-07-22 11:51:13 +0700

    fix lh

--- resources/views/emails/lichhen.blade.php
+++ resources/views/emails/lichhen.blade.php
@@ -1,5 +1,7 @@
 <h2>Xin chào {{ $lichhen->khachhang->taikhoan->hoten }}</h2>
-<p>Bạn đã đặt lịch khám thành công với bác sĩ: {{ $lichhen->nhanvien->taikhoan->hoten }}</p>
+@if ($lichhen->nhanvien)
+    <p>Bạn đã đặt lịch khám thành công với bác sĩ: {{ $lichhen->nhanvien->taikhoan->hoten }}</p>
+@endif
 <p>Ngày hẹn: {{ $lichhen->ngayhen }}</p>
 <p>Ca khám: {{ $lichhen->cakham->khunggio }}</p>
 <p>Trạng thái: {{ $lichhen->trangthai }}</p>
